45269a1f9ca832acb900ed036c7a003e62b9f562
kalikovar <kalikovar@gmail.com>
2024-11-07 19:33:53 +0100

    au sound test

--- .github/scripts/setup_audio.sh
+++ .github/scripts/setup_audio.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-set -xeo pipefail
+#set -xeo pipefail
 
-sudo apt update
-sudo apt install -y pulseaudio libportaudio2 dbus-x11 libasound-dev
+sudo apt-get update
+sudo apt-get install -y pulseaudio libportaudio2 dbus-x11 libasound-dev
 systemctl --user restart pulseaudio.service
 systemctl --user restart pulseaudio.socket
 pactl list
