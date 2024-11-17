6ac6daf6b6f1da6c4d0f0c622850ac76206eefdf
kalikovar <kalikovar@gmail.com>
2024-11-17 14:29:41 +0100

    au sound test

--- .github/scripts/setup_sound.sh
+++ .github/scripts/setup_sound.sh
@@ -3,6 +3,7 @@
 sudo apt-get update
 sudo apt-get install -y libportaudio2 dbus-x11 libasound-dev
 sudo apt-get install -y kmod
+sudo apt-get install -y alsa-utils
 #sudo apt-get install -y pulseaudio libportaudio2 dbus-x11 libasound-dev
 #systemctl --user restart pulseaudio.service
 #systemctl --user restart pulseaudio.socket
@@ -10,5 +11,5 @@ sudo apt-get install -y kmod
 
 sudo modprobe snd_dummy
 
-pactl list
+#pactl list
 aplay -l
