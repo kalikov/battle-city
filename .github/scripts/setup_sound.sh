e5b8184e892b2c42bb43469ddb392f3b88aaa46a
kalikovar <kalikovar@gmail.com>
2024-11-17 14:23:54 +0100

    au sound test

--- .github/scripts/setup_sound.sh
+++ .github/scripts/setup_sound.sh
@@ -2,6 +2,7 @@
 
 sudo apt-get update
 sudo apt-get install -y libportaudio2 dbus-x11 libasound-dev
+sudo apt-get install -y kmod
 #sudo apt-get install -y pulseaudio libportaudio2 dbus-x11 libasound-dev
 #systemctl --user restart pulseaudio.service
 #systemctl --user restart pulseaudio.socket
